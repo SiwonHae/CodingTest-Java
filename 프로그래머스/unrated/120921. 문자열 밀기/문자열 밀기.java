b64d3bc2e4a44cf1f5484e1367a604e7a8ce3442
JuHyuk Lim <705lo@naver.com>
2024-01-11 16:17:23 +0900

    [level unrated] Title: 문자열 밀기, Time: 1.18 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+
+        for (int i = 0; i < A.length(); i++) {
+            if (A.equals(B)) {
+                return answer;
+            }
+
+            String endChar = A.substring(A.length() - 1);
+            A = endChar + A.substring(0, A.length() - 1);
+            answer++;
+        }
+
+        return -1;
+    }
+}
