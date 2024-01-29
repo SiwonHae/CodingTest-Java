ab6fcc4e78dbfae309abe31ab556657bf2c4820b
JuHyuk Lim <705lo@naver.com>
2024-01-29 22:26:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,60 +1,38 @@
-class Solution {
+import java.util.*;
 
-    public int searchIdNumber(String[] id_list, String name) {
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+
+        Map<String, Integer> idMap = new HashMap<>();
+        // 이름 : 인덱스 번호
         // muzi : 0
         // frodo : 1
         // apeach : 2
         // neo : 3
 
+        Map<String, HashSet<String>> reportInfo = new HashMap<>();
+        // 신고 당한 이름 : 신고한 사람 이름
+        // muzi : {appeach} -> 무지를 신고한 사람이 appeach임.
+
         for (int i = 0; i < id_list.length; i++) {
-            if (id_list[i].equals(name)) {
-                return i;
-            }
+            String name = id_list[i];
+            idMap.put(name, i);
+            reportInfo.put(name, new HashSet<>());
         }
 
-        // 실패하지 않음.
-        return 0;
-    }
-
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-
-        int[][] reportMatrix = new int[id_list.length][id_list.length];
         for (int i = 0; i < report.length; i++) {
             String reporter = report[i].split(" ")[0];
             String target = report[i].split(" ")[1];
-            int reporterNum = searchIdNumber(id_list, reporter);
-            int targetNum = searchIdNumber(id_list, target);
-            reportMatrix[reporterNum][targetNum]++;
+            reportInfo.get(target).add(reporter);
         }
 
-        int[] reportedNum = new int[id_list.length]; // 신고 당한 횟수
-        for (int i = 0; i < reportMatrix.length; i++) {
-            for (int j = 0; j < reportMatrix.length; j++) {
-                if (reportMatrix[i][j] > 0) {
-                    reportedNum[j]++;
+        for (String key : reportInfo.keySet()) {
+            if (reportInfo.get(key).size() >= k) {
+                for (String id : reportInfo.get(key)) {
+                    answer[idMap.get(id)]++;
                 }
             }
-        }
-
-        int[] bannedNum = new int[id_list.length]; // 정지 당한 아이디 번호
-        for (int i = 0; i < reportedNum.length; i++) {
-            if (reportedNum[i] >= k) {
-                bannedNum[i] = 1;
-            }
-        }
-
-        for (int i = 0; i < reportMatrix.length; i++) {
-            int cnt = 0; // 결과 메일 수
-            for (int j = 0; j < reportMatrix.length; j++) {
-                if (reportMatrix[i][j] > 0) {
-                    // 내가 신고한 사람이 정지 되었는지 확인
-                    if (bannedNum[j] == 1) {
-                        cnt++; // 정지 되었다면 메일을 받자.
-                    }
-                }
-            }
-            answer[i] = cnt;
         }
 
 
