8cb5ad8791a19de3d30f126ca2a0cc39c296d52b
JuHyuk Lim <705lo@naver.com>
2024-05-06 17:24:57 +0900

    [level 2] Title: 삼각 달팽이, Time: 22.38 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,4 +1,10 @@
 class Solution {
+
+    // x축 변화량 (아래, 오른쪽, 왼쪽 위)
+    private static final int[] dx = {0, 1, -1};
+    // y축 변화량 (아래, 오른쪽, 왼쪽 위)
+    private static final int[] dy = {1, 0, -1};
+
     public int[] solution(int n) {
         // 밑변의 길이와 높이가 n인 삼각형
         // n = 6
@@ -9,34 +15,37 @@ class Solution {
         // 5  18  19  20  12
         // 6   7   8   9  10  11
 
-        int[] answer = new int[(n * (n + 1)) / 2];
-
         int[][] board = new int[n][n];
 
-        int y = -1;
         int x = 0;
-        int value = 1;
+        int y = 0;
+        int d = 0; // dx, dy를 위한 방향 변수
+        int v = 1;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                // 아래로
-                if (i % 3 == 0) {
-                    y++;
-                } else if (i % 3 == 1) {
-                    // 오른쪽으로
-                    x++;
-                } else if (i % 3 == 2) {
-                    // 왼쪽 위 대각선으로
-                    y--;
-                    x--;
+        while (true) {
+            board[y][x] = v++;
+
+            int nx = x + dx[d]; // 다음 x 위치
+            int ny = y + dy[d]; // 다음 y 위치
+
+            if (nx == n || ny == n || nx == -1 || ny == -1 || board[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+
+                if (nx == n || ny == n || nx == -1 || ny == -1 || board[ny][nx] != 0) {
+                    break;
                 }
-                board[y][x] = value++;
             }
+
+            x = nx;
+            y = ny;
         }
 
+        int[] answer = new int[v - 1];
         int idx = 0;
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j <= i; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 answer[idx++] = board[i][j];
             }
         }
