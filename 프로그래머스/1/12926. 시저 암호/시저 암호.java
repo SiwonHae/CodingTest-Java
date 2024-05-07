bb1b81782e1e92f01548a884f8ed99499b035348
JuHyuk Lim <705lo@naver.com>
2024-05-07 15:41:38 +0900

    [level 1] Title: 시저 암호, Time: 1.81 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,34 +1,28 @@
 class Solution {
+
+    private char push(char c, int n) {
+        if (!Character.isAlphabetic(c)) {
+                return c;
+        }
+
+        // 대문자면 'A', 소문자면 'a'
+        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        int pos = c - offset;
+        pos = (pos + n) % 26;
+
+        return (char) (offset + pos);
+    }
+
     public String solution(String s, int n) {
         String answer = "";
 
-        int diff = 'z' - 'a';
-
-        String[] str = s.split("");
-        for (int i = 0; i < s.length(); i++) {
-            if (str[i].equals(" ")) {
-                answer += " ";
-            } else {
-
-                // 소문자 일 때
-                if (str[i].charAt(0) >= 'a' && str[i].charAt(0) <= 'z') {
-                    if (str[i].charAt(0) + n > 'z') {
-                        answer += (char) (str[i].charAt(0) + n - diff - 1);
-                    } else {
-                        answer += (char) (str[i].charAt(0) + n);
-                    }
-                }
-
-                // 대문자 일 때
-                if (str[i].charAt(0) >= 'A' && str[i].charAt(0) <= 'Z') {
-                    if (str[i].charAt(0) + n > 'Z') {
-                        answer += (char) (str[i].charAt(0) + n - diff - 1);
-                    } else {
-                        answer += (char) (str[i].charAt(0) + n);
-                    }
-                }
-            }
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            char newC = push(c, n);
+            sb.append(newC);
         }
+
+        answer = sb.toString();
 
         return answer;
     }
