83858298cd82a8a120986b04df3f2956a0afba03
JuHyuk Lim <705lo@naver.com>
2024-06-30 18:41:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 988 ms, Memory: 97124 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] result;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int cnt;
+
+    public static void dfs(int r) {
+        visited[r] = true;
+        result[r] = cnt++;
+
+        for (int x : graph.get(r)) {
+            if (!visited[x]) {
+                dfs(x);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        result = new int[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        cnt = 1;
+        dfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(result[i] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
