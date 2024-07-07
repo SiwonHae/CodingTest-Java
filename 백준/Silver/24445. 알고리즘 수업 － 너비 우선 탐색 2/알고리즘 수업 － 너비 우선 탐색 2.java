6e8e17796e6a95e90ca74e7d0bd0a9596879e7be
JuHyuk Lim <705lo@naver.com>
2024-07-07 14:58:32 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1120 ms, Memory: 97792 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] result;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int cnt = 1;
+
+    public static void bfs(int r) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(r);
+        visited[r] = true;
+        result[r] = cnt++;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            Collections.sort(graph.get(u), Collections.reverseOrder());
+
+            for (int i = 0; i < graph.get(u).size(); i++) {
+                int v = graph.get(u).get(i);
+
+                if (!visited[v]) {
+                    visited[v] = true;
+                    result[v] = cnt++;
+                    queue.add(v);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        result = new int[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        bfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(result[i] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
