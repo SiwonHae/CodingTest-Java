8f07cbc8489e34145b8c9cb3e3410a4f95c5a901
JuHyuk Lim <705lo@naver.com>
2024-08-01 16:13:48 +0900

    [Bronze I] Title: ROT13, Time: 128 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -2,33 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        String S = br.readLine();
 
-            if (c >= 'a' && c <= 'z') {
-                if (c + 13 <= 'z') {
-                    sb.append((char) (c + 13));
-                } else {
-                    c = (char) (c + 13 - 'z' + 'a' - 1);
-                    sb.append(c);
-                }
-            } else if (c >= 'A' && c <= 'Z') {
-                if (c + 13 <= 'Z') {
-                    sb.append((char) (c + 13));
-                } else {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+
+            // 대문자
+            if (c >= 'A' && c <= 'Z') {
+                if (c + 13 > 'Z') {
                     c = (char) (c + 13 - 'Z' + 'A' - 1);
-                    sb.append(c);
+                } else {
+                    c += 13;
                 }
-            } else {
-                sb.append(c);
+            } else if (c >= 'a' && c <= 'z') { // 소문자
+                if (c + 13 > 'z') {
+                    c = (char) (c + 13 - 'z' + 'a' - 1);
+                } else {
+                    c += 13;
+                }
             }
+
+            sb.append(c);
         }
 
         bw.write(sb.toString());
