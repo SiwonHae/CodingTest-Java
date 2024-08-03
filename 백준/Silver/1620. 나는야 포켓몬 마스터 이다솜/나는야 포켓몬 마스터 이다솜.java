82c6f1e13bee02374190888f4739117079232bfe
JuHyuk Lim <705lo@naver.com>
2024-08-03 15:36:50 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 528 ms, Memory: 58228 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,37 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        Map<Integer, String> map1 = new HashMap<>();
-        Map<String , Integer> map2 = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        Map<Integer, String> map2 = new HashMap<>();
 
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            map1.put(i + 1, name);
-            map2.put(name, i + 1);
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            map.put(name, i + 1);
+            map2.put(i + 1, name);
         }
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
+        for (int i = 0; i < M; i++) {
+            String cmd = br.readLine();
 
             if (Character.isDigit(cmd.charAt(0))) {
-                bw.write(map1.get(Integer.parseInt(cmd))+ "\n");
+                bw.write(map2.get(Integer.parseInt(cmd)) + "\n");
             } else {
-                bw.write(map2.get(cmd) + "\n");
+                bw.write(map.get(cmd) + "\n");
             }
-
         }
-
+        
         bw.flush();
     }
 }
