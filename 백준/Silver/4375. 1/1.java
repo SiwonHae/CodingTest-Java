c05454495c93e893af41875d6a0a4ab65b9d6a10
JuHyuk Lim <705lo@naver.com>
2024-08-03 16:29:39 +0900

    [Silver III] Title: 1, Time: 116 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -2,20 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
         String input = "";
         while ((input = br.readLine()) != null) {
-            int n = Integer.parseInt(input);
+            int num = Integer.parseInt(input);
 
-            int start = 1;
+            int number = 1;
             int cnt = 1;
-            // 1 11 111 1111 등
-            while (start % n != 0) {
-                start = (start * 10 + 1) % n;
+            while (number % num != 0) {
+                number = (number * 10 + 1) % num;
                 cnt++;
             }
 
