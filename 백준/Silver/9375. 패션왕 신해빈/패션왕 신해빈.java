d4008b6a91672d99a6f39fc1cab5900e1386e821
JuHyuk Lim <705lo@naver.com>
2024-08-03 15:54:04 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 116 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,29 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
+            Map<String, Integer> kindMap = new HashMap<>();
 
-            // 맵의 사이즈 => 의상의 종류
-            Map<String, Integer> map = new HashMap<>();
-
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(br.readLine());
                 String name = st.nextToken();
                 String kind = st.nextToken();
-                map.put(kind, map.getOrDefault(kind, 0) + 1);
+                kindMap.put(kind, kindMap.getOrDefault(kind, 0) + 1);
             }
 
             int result = 1;
-            for (int cnt : map.values()) {
+            for (int cnt : kindMap.values()) {
                 result *= (cnt + 1);
             }
             result--;
