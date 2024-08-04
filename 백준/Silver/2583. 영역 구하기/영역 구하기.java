b8f503677707e106feb3ed1902f70e9503031aed
JuHyuk Lim <705lo@naver.com>
2024-08-04 20:45:44 +0900

    [Silver I] Title: 영역 구하기, Time: 144 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -2,29 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
     static int[][] board;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    static int n;
-    static int m;
-    static int k;
-    static int area;
-    static List<Integer> areaList = new ArrayList<>();
+    static int N, M, K;
+    static int cnt = 0;
+    static int area = 1;
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        board = new int[m][n];
-        visited = new boolean[m][n];
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < k; i++) {
+        board = new int[M][N];
+        visited = new boolean[M][N];
+
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
@@ -38,43 +37,46 @@ public class Main {
             }
         }
 
-        int cnt = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
                 if (board[i][j] == 0 && !visited[i][j]) {
-                    area = 1;
                     dfs(i, j);
-                    areaList.add(area);
+                    cnt++;
+                    list.add(area);
+                    area = 1;
                 }
             }
         }
 
-        Collections.sort(areaList);
+        bw.write(cnt + "\n");
 
-        bw.write(areaList.size() + "\n");
-        for (int a : areaList) {
-            bw.write(a + " ");
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+            bw.write(list.get(i) + " ");
         }
+
         bw.flush();
     }
 
-    public static void dfs(int x, int y) {
-        if (visited[x][y]) {
+    public static void dfs(int y, int x) {
+
+        if (visited[y][x]) {
             return;
         }
 
-        visited[x][y] = true;
+        visited[y][x] = true;
 
         for (int i = 0; i < 4; i++) {
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
+            int ny = y + dy[i];
+            int nx = x + dx[i];
 
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                if (board[nx][ny] == 0 && !visited[nx][ny]) {
+            if (ny >= 0 && ny < M && nx >= 0 && nx < N) {
+                if (board[ny][nx] == 0 && !visited[ny][nx]) {
+                    dfs(ny, nx);
                     area++;
-                    dfs(nx, ny);
                 }
             }
         }
+
     }
 }
