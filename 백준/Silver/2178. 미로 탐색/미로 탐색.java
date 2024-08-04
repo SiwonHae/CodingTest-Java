95d56eeef56becf560faa7189a27c23d39cd1d65
JuHyuk Lim <705lo@naver.com>
2024-08-04 19:47:17 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,85 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
     static int[][] board;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-//    static int result = Integer.MAX_VALUE;
-    static int n;
-    static int m;
+    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        board = new int[n][m];
-        visited = new boolean[n][m];
+        board = new int[N][M];
+        visited = new boolean[N][M];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String s = st.nextToken();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = s.charAt(j) - '0';
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = input.charAt(j) - '0';
             }
         }
 
         bfs(0, 0);
 
-        bw.write(String.valueOf(board[n - 1][m - 1]));
+        bw.write(String.valueOf(board[N - 1][M - 1]));
         bw.flush();
     }
 
-    public static void bfs(int startX, int startY) {
-        Queue<int []> queue = new LinkedList<>();
-        queue.add(new int[] {startX, startY});
-
-        visited[startX][startY] = true;
+    public static void bfs(int startY, int startX) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {startY, startX});
+        visited[startY][startX] = true;
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int y = current[0];
+            int x = current[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
                 int ny = y + dy[i];
+                int nx = x + dx[i];
 
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (board[nx][ny] == 1 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        board[nx][ny] = board[x][y] + 1;
-                        queue.offer(new int[]{nx, ny});
+                if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+                    if (!visited[ny][nx] && board[ny][nx] == 1) {
+                        board[ny][nx] = board[y][x] + 1;
+                        visited[ny][nx] = true;
+                        queue.add(new int[] {ny, nx});
                     }
                 }
             }
         }
     }
-
-
-//    public static void dfs(int x, int y, int dist) {
-//
-//        if (x == n - 1 && y == m - 1) {
-//            result = Math.min(result, dist);
-//            return;
-//        }
-//
-//        visited[x][y] = true;
-//
-//        for (int i = 0; i < 4; i++) {
-//            int nx = x + dx[i];
-//            int ny = y + dy[i];
-//
-//            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && board[nx][ny] == 1) {
-//                dfs(nx, ny, dist + 1);
-//            }
-//        }
-//
-//        visited[x][y] = false;
-//    }
 }
