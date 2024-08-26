a935a55ff338d6d2cad7d9a35e5cc0ceaf877bd1
JuHyuk Lim <705lo@naver.com>
2024-08-26 14:41:25 +0900

    [Gold III] Title: 순회강연, Time: 284 ms, Memory: 20216 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Lecture> pq = new PriorityQueue<>(new Comparator<Lecture>() {
+            @Override
+            public int compare(Lecture o1, Lecture o2) {
+                if (o1.pay == o2.pay) {
+                    return o2.day - o1.day;
+                }
+
+                return o2.pay - o1.pay;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int pay = Integer.parseInt(st.nextToken());
+            int day = Integer.parseInt(st.nextToken());
+            pq.add(new Lecture(pay, day));
+        }
+
+        boolean[] visited = new boolean[10001];
+        int result = 0;
+
+        while (!pq.isEmpty()) {
+            Lecture lecture = pq.poll();
+
+            for (int i = lecture.day; i > 0; i--) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    result += lecture.pay;
+                    break;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+
+}
+
+class Lecture {
+    int pay;
+    int day;
+
+    public Lecture(int pay, int day) {
+        this.pay = pay;
+        this.day = day;
+    }
+}
