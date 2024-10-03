f25efbe93dd430ecad61129d8c9a94401cbc23cf
JuHyuk Lim <705lo@naver.com>
2024-10-03 23:28:18 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 500 ms, Memory: 53732 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,26 +10,25 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
-        Map<Integer, String> map2 = new HashMap<>();
-
+        Map<String, Integer> m1 = new HashMap<>();
+        Map<Integer, String> m2 = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             String name = br.readLine();
-            map.put(name, i + 1);
-            map2.put(i + 1, name);
+            m1.put(name, i + 1);
+            m2.put(i + 1, name);
         }
 
         for (int i = 0; i < M; i++) {
             String cmd = br.readLine();
 
-            if (Character.isDigit(cmd.charAt(0))) {
-                bw.write(map2.get(Integer.parseInt(cmd)) + "\n");
+            if (Character.isLetter(cmd.charAt(0))) {
+                bw.write(m1.get(cmd) + "\n");
             } else {
-                bw.write(map.get(cmd) + "\n");
+                bw.write(m2.get(Integer.parseInt(cmd)) + "\n");
             }
         }
-        
+
         bw.flush();
     }
 }
