93c861b021bc66717e42b5c34e06e2ff73418b51
JuHyuk Lim <705lo@naver.com>
2024-10-03 23:10:30 +0900

    [Silver III] Title: 수열, Time: 976 ms, Memory: 23820 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -7,29 +7,29 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int max = Integer.MIN_VALUE;
-        int[] arr = new int[n];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
+        int result = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n - k + 1; i++) {
-            int start = i;
+        for (int i = 0; i < N - K + 1; i++) {
             int sum = 0;
-            for (int j = 0; j < k; j++) {
-                sum += arr[start];
-                start++;
+            for (int j = i; j < i + K; j++) {
+                sum += a[j];
             }
-            max = Math.max(max, sum);
+
+            result = Math.max(result, sum);
         }
 
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(result));
         bw.flush();
     }
 }
