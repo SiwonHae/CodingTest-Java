a9f28c9d8243dab45898432ff28b246565540026
JuHyuk Lim <705lo@naver.com>
2024-10-03 21:44:48 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -2,21 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
         String S = br.readLine();
+
         int[] arr = new int[26];
 
         for (int i = 0; i < S.length(); i++) {
             arr[S.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i < 26; i++) {
-            bw.write(arr[i] + " ");
+        for (int i : arr) {
+            bw.write(String.valueOf(i) + " ");
         }
 
         bw.flush();
