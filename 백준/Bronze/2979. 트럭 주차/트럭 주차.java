8f92f2353895db0edceeb9d0bf9315fdf9f30b2c
JuHyuk Lim <705lo@naver.com>
2024-10-03 21:57:44 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,10 +11,11 @@ public class Main {
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        int[] time = new int[100];
+        int[] time = new int[102];
 
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
+
             int arrive = Integer.parseInt(st.nextToken());
             int leave = Integer.parseInt(st.nextToken());
 
@@ -25,7 +25,8 @@ public class Main {
         }
 
         int result = 0;
-        for (int i = 0; i < 100; i++) {
+
+        for (int i = 0; i < time.length; i++) {
             if (time[i] == 1) {
                 result += A;
             } else if (time[i] == 2) {
