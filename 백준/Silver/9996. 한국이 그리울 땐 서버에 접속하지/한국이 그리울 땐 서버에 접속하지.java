f6305e7170762939ddabe6724a1b07730221663d
JuHyuk Lim <705lo@naver.com>
2024-10-03 22:44:42 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 124 ms, Memory: 14772 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,26 +1,25 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        String[] pattern = br.readLine().split("\\*");
-        Pattern p = Pattern.compile(pattern[0] + ".*" + pattern[1]);
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            Matcher m = p.matcher(s);
-            if (m.matches()) {
+        String pattern = br.readLine().replace("*", ".*");
+
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+
+            if (Pattern.matches(pattern, name)) {
                 bw.write("DA\n");
             } else {
                 bw.write("NE\n");
             }
+
         }
 
         bw.flush();
