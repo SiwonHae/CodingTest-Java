923c0e79ab1c3dc8aa2c40709bc6988ddab4d027
JuHyuk Lim <705lo@naver.com>
2024-10-03 23:59:33 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,33 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st;
-        for (int i = 0; i < t; i++) {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+
+            Map<String, Integer> map = new HashMap<>();
+
             int n = Integer.parseInt(br.readLine());
 
-            Map<String, Integer> kindMap = new HashMap<>();
-
             for (int j = 0; j < n; j++) {
-                st = new StringTokenizer(br.readLine());
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 String name = st.nextToken();
                 String kind = st.nextToken();
-                kindMap.put(kind, kindMap.getOrDefault(kind, 0) + 1);
+
+                map.put(kind, map.getOrDefault(kind, 0) + 1);
             }
 
             int result = 1;
-            for (int cnt : kindMap.values()) {
-                result *= (cnt + 1);
+            for (int value : map.values()) {
+                result *= value + 1;
             }
-            result--;
 
-            bw.write(result + "\n");
+            bw.write(String.valueOf(result - 1) + "\n");
         }
 
         bw.flush();
