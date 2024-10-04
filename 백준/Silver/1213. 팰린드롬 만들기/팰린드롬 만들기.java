c8c83e6788ec9dea81b452e87d3495dda71a813c
JuHyuk Lim <705lo@naver.com>
2024-10-04 14:13:22 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 140 ms, Memory: 16556 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -2,54 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String name = br.readLine();
+        String s = br.readLine();
 
-        int[] arr = new int[26];
+        int[] a = new int[26];
         int oddCnt = 0;
-        int middle = 0;
-        // 홀수인 알파벳이 2개 이상이면 펠린드롬 불가.
-        // 홀수인 알파벳이 중간에 들어가야 펠린드롬이 된다.
+        char oddChar = 0;
 
-        for (int i = 0; i < name.length(); i++) {
-            arr[name.charAt(i) - 'A']++;
+        for (int i = 0; i < s.length(); i++) {
+            a[s.charAt(i) - 'A']++;
         }
 
         for (int i = 0; i < 26; i++) {
-            if (arr[i] % 2 != 0) {
+            if (a[i] % 2 != 0) {
                 oddCnt++;
-                middle = i;
+                oddChar = (char) (i + 'A');
+            }
+
+            if (oddCnt >= 2) {
+                bw.write("I'm Sorry Hansoo");
+                bw.flush();
+                return;
             }
         }
 
-        if (oddCnt >= 2) {
-            bw.write("I'm Sorry Hansoo");
-            bw.flush();
-            return;
-        }
+        String result = "";
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder front = new StringBuilder();
 
         for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < arr[i] / 2; j++) {
-                sb.append((char) (i + 'A'));
+            if (a[i] != 0) {
+                for (int j = 0; j < a[i] / 2; j++) {
+                    front.append((char) (i + 'A'));
+                }
             }
         }
 
-        String front = sb.toString();
-
-        if (oddCnt == 1) {
-            front += (char) (middle + 'A');
+        if (oddCnt == 0) {
+            result = front.toString() + front.reverse().toString();
+        } else {
+            result = front.toString() + oddChar + front.reverse().toString();
         }
 
-        String back = sb.reverse().toString();
-
-        bw.write(front + back);
+        bw.write(result);
         bw.flush();
     }
 }
