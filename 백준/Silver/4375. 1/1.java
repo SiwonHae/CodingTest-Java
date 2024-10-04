8eda888c9a8f1a9be87765e65e9b980132e3bfe5
JuHyuk Lim <705lo@naver.com>
2024-10-04 14:39:51 +0900

    [Silver III] Title: 1, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,14 +10,16 @@ public class Main {
         while ((input = br.readLine()) != null) {
             int num = Integer.parseInt(input);
 
-            int number = 1;
-            int cnt = 1;
-            while (number % num != 0) {
-                number = (number * 10 + 1) % num;
-                cnt++;
+            // 3의 배수 중 가장 각 자릿수가 모두 1인 가장 작은 수 111
+
+            int target = 1;
+            int result = 1;
+            while (target % num != 0) {
+                target = (target * 10 + 1) % num;
+                result++;
             }
 
-            bw.write(cnt + "\n");
+            bw.write(String.valueOf(result) + "\n");
         }
 
         bw.flush();
