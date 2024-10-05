67dab5c3b033dde04c5542a7730bad5b007ced84
JuHyuk Lim <705lo@naver.com>
2024-10-05 15:29:18 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -6,18 +6,17 @@ public class Main {
     static int[] dx = {0, 0, -1, 1};
     static int[][] board;
     static boolean[][] visited;
-    static int N, M, K;
-    static int result = 0;
+    static int T, M, N, K;
+    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
@@ -30,6 +29,7 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
+
                 board[y][x] = 1;
             }
 
@@ -42,7 +42,7 @@ public class Main {
                 }
             }
 
-            bw.write(result + "\n");
+            bw.write(String.valueOf(result) + "\n");
         }
 
         bw.flush();
@@ -60,11 +60,14 @@ public class Main {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
-                if (board[ny][nx] == 1 && !visited[ny][nx]) {
-                    dfs(ny, nx);
-                }
+            if (ny < 0 || ny >= N || nx < 0 || nx >= M) {
+                continue;
+            }
+
+            if (!visited[ny][nx] && board[ny][nx] == 1) {
+                dfs(ny, nx);
             }
         }
+
     }
 }
