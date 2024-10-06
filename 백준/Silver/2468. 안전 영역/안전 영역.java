f013c2a006760414b25fbcaeb924b9c789b542a3
JuHyuk Lim <705lo@naver.com>
2024-10-06 15:17:06 +0900

    [Silver I] Title: 안전 영역, Time: 204 ms, Memory: 18428 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,38 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+
+    static int N;
     static int[][] board;
     static boolean[][] visited;
-    static int N;
-    static int maxHeight = 0;
-    static int cnt = 0;
-    static int result = 0;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int result = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
 
         board = new int[N][N];
 
+        int maxHeight = 0;
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                maxHeight = Math.max(board[i][j], maxHeight);
+                maxHeight = Math.max(maxHeight, board[i][j]);
             }
         }
 
         for (int i = 1; i <= maxHeight; i++) {
+            int cnt = 0;
             visited = new boolean[N][N];
 
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    if (board[j][k] >= i && !visited[j][k]) {
+                    if (board[j][k] > i && !visited[j][k]) {
                         dfs(j, k, i);
                         cnt++;
                     }
@@ -41,14 +42,13 @@ public class Main {
             }
 
             result = Math.max(result, cnt);
-            cnt = 0;
         }
 
         bw.write(String.valueOf(result));
         bw.flush();
     }
 
-    public static void dfs(int y, int x, int height) {
+    public static void dfs(int y, int x, int maxHeight) {
 
         if (visited[y][x]) {
             return;
@@ -60,11 +60,14 @@ public class Main {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
-                if (board[ny][nx] >= height && !visited[ny][nx]) {
-                    dfs(ny, nx, height);
-                }
+            if (ny < 0 || ny >= N || nx < 0 || nx >= N) {
+                continue;
+            }
+
+            if (board[ny][nx] > maxHeight && !visited[ny][nx]) {
+                dfs(ny, nx, maxHeight);
             }
         }
+
     }
 }
