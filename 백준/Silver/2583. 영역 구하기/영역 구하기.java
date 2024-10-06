9495902968961fc15865f3be13636263edc5fa04
JuHyuk Lim <705lo@naver.com>
2024-10-06 15:46:54 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 16708 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+
+    static int M, N, K;
     static int[][] board;
     static boolean[][] visited;
-    static int N, M, K;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
     static int cnt = 0;
     static int area = 1;
-    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,16 +22,17 @@ public class Main {
 
         board = new int[M][N];
         visited = new boolean[M][N];
+        List<Integer> areas = new ArrayList<>();
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int lx = Integer.parseInt(st.nextToken());
+            int ly = Integer.parseInt(st.nextToken());
+            int rx = Integer.parseInt(st.nextToken()) - 1;
+            int ry = Integer.parseInt(st.nextToken()) - 1;
 
-            for (int j = y1; j < y2; j++) {
-                for (int k = x1; k < x2; k++) {
+            for (int j = ry; j >= ly; j--) {
+                for (int k = rx; k >= lx; k--) {
                     board[j][k] = 1;
                 }
             }
@@ -39,10 +40,10 @@ public class Main {
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 0 && !visited[i][j]) {
+                if (board[i][j] != 1 && !visited[i][j]) {
                     dfs(i, j);
+                    areas.add(area);
                     cnt++;
-                    list.add(area);
                     area = 1;
                 }
             }
@@ -50,31 +51,37 @@ public class Main {
 
         bw.write(cnt + "\n");
 
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            bw.write(list.get(i) + " ");
+        Collections.sort(areas);
+        for (int i = 0; i < areas.size(); i++) {
+            bw.write(areas.get(i) + " ");
         }
+
 
         bw.flush();
     }
 
     public static void dfs(int y, int x) {
-
         if (visited[y][x]) {
             return;
         }
 
         visited[y][x] = true;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4 ; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
 
-            if (ny >= 0 && ny < M && nx >= 0 && nx < N) {
-                if (board[ny][nx] == 0 && !visited[ny][nx]) {
-                    dfs(ny, nx);
-                    area++;
-                }
+            if (ny < 0 || ny >= M || nx < 0 || nx >= N) {
+                continue;
+            }
+
+            if (visited[ny][nx]) {
+                continue;
+            }
+
+            if (board[ny][nx] != 1) {
+                dfs(ny, nx);
+                area++;
             }
         }
 
