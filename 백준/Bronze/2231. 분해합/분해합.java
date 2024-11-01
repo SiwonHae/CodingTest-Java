a56a5a9cbeb34102277f32040a68b4eb46970140
JuHyuk Lim <705lo@naver.com>
2024-11-01 13:36:41 +0900

    [Bronze II] Title: 분해합, Time: 1528 ms, Memory: 307956 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -5,20 +5,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i < n; i++) {
-            String number = String.valueOf(i);
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
             String[] num = String.valueOf(i).split("");
 
-            int sum = Integer.parseInt(number);
+            int sum = i;
             for (int j = 0; j < num.length; j++) {
                 sum += Integer.parseInt(num[j]);
             }
 
-            if (sum == n) {
+            if (sum == N) {
                 bw.write(String.valueOf(i));
                 bw.flush();
                 return;
