e1708e9a553a1a71ea8d42af89e41cb0adbcaa4b
JuHyuk Lim <705lo@naver.com>
2024-11-14 17:33:05 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static List<Integer> list = new ArrayList<>();
+    static List<Integer> result = new ArrayList<>();
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> resultSet = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        dfs(0, 0);
+
+        for (String s : resultSet) {
+            bw.write(s + "\n");
+        }
+        bw.flush();
+    }
+
+    public static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result.get(i) + " ");
+            }
+            resultSet.add(sb.toString());
+            sb = new StringBuilder();
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result.add(list.get(i));
+                dfs(depth + 1, i);
+                result.remove(result.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
