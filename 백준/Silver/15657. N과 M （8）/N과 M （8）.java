61b7216546c0fb65619896252ea08fe9166b8645
JuHyuk Lim <705lo@naver.com>
2024-11-14 17:21:47 +0900

    [Silver III] Title: N과 M (8), Time: 184 ms, Memory: 23776 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static List<Integer> list = new ArrayList<>();
+    static List<Integer> result = new ArrayList<>();
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        dfs(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    public static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result.get(i) + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            result.add(list.get(i));
+            dfs(depth + 1, i);
+            result.remove(result.size() - 1);
+        }
+    }
+}
