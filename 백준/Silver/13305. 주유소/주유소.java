1a680bc032386599ad459f34cb790633eb384e80
JuHyuk Lim <705lo@naver.com>
2024-11-20 16:10:27 +0900

    [Silver III] Title: 주유소, Time: 424 ms, Memory: 41660 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,40 +9,28 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> distance = new ArrayList<>();
+        List<Long> distance = new ArrayList<>();
         for (int i = 0; i < N - 1; i++) {
-            distance.add(Integer.parseInt(st.nextToken()));
+            distance.add(Long.parseLong(st.nextToken()));
         }
 
         st = new StringTokenizer(br.readLine());
-        List<Integer> price = new ArrayList<>();
+        List<Long> price = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            price.add(Integer.parseInt(st.nextToken()));
+            price.add(Long.parseLong(st.nextToken()));
         }
 
         // 기름을 넣을 수 있는 곳은 0 ~ N-1번 까지.
         // 무조건 0번쨰에서 기름을 넣어야 2번째 도시로 넘어갈 수 있음.
+        // 현재 주유소의 가격이 다음 주유소의 가격보다 비싸면 최소로만 넣고 다음 도시로 이동하자.
+        // 현재 주유소의 가격이 다음 주유소의 가격보다 싸다면 현재 주유소보다 비싼 주유소를 찾을 때 까지의 거리 만큼의 리터를 넣자.
 
-        int result = 0;
+        long min = price.get(0);
+        long result = 0;
+
         for (int i = 0; i < N - 1; i++) {
-            int distanceIdx = i;
-
-            // 현재 주유소의 가격이 다음 주유소의 가격보다 비싸면 최소로만 넣고 다음 도시로 이동하자.
-            if (price.get(i) > price.get(i + 1)) {
-                result += price.get(i) * distance.get(i);
-            } else {
-                // 현재 주유소의 가격이 다음 주유소의 가격보다 싸다면 현재 주유소보다 비싼 주유소를 찾을 때 까지의 거리 만큼의 리터를 넣자.
-                int sumDistance = 0;
-                for (int j = i; j < N - 1; j++) {
-                    if (price.get(i) > price.get(j)) {
-                        break;
-                    }
-                    sumDistance += distance.get(j);
-                    distanceIdx++;
-                }
-                result += price.get(i) * sumDistance;
-                i = distanceIdx;
-            }
+            result += min * distance.get(i);
+            min = Math.min(min, price.get(i + 1));
         }
 
         bw.write(String.valueOf(result));
