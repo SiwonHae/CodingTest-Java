5df938bea8eec28e6f6ced6307c9a7a3786f12c5
JuHyuk Lim <705lo@naver.com>
2024-12-06 12:27:02 +0900

    [level 1] Title: 폰켓몬, Time: 5.89 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,28 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-
-        // N/2 개 선택.
-        int select = nums.length / 2;
-
-        List<Integer> cnt = new ArrayList<>(); // 종류 번호의 개수
-        for (int i = 0; i < nums.length; i++) {
-            if (!cnt.contains(nums[i])) {
-                cnt.add(nums[i]);
-            }
+        
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            set.add(i);
         }
-        int size = cnt.size();
-
-        if (size <= select) {
-            answer = size;
-        } else {
-            answer = select;
-        }
-
-
+        
+        int pick = nums.length / 2;
+        
+        answer = Math.min(pick, set.size());
+        
         return answer;
     }
 }
