5a5c796e50d521d74ba6f0b65068d884eeacd33d
JuHyuk Lim <705lo@naver.com>
2024-12-13 13:09:41 +0900

    [level 2] Title: 프로세스, Time: 0.95 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,40 +3,30 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-
-        // A B C D
-        // 2 1 3 2
-        // C D A B 순서로 실행됨.
-
-        List<Integer> queue = new LinkedList<>();
+        
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int run = 0;
+        
         for (int i : priorities) {
-            queue.add(i);
+            pq.offer(i);
         }
-
-        while (location >= 0) {
-
-            int max = Collections.max(queue);
-
-            int process = queue.get(0); // 제일 앞을 꺼낸다.
-            queue.remove(0);
-
-            if (process >= max) {
-                location--;
-                answer++;
-
-                if (location < 0) {
-                    break;
-                }
-            } else {
-                queue.add(process);
-                location--;
-                if (location < 0) {
-                    location = queue.size() - 1;
+        
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                int current = priorities[i];
+                
+                if (current == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    
+                    if (i == location) {
+                        return answer;
+                    }
                 }
             }
         }
-
-
+        
         return answer;
     }
 }
