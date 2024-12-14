c93aa2c83d64e80b2b9fe6bce170b2561e39870a
JuHyuk Lim <705lo@naver.com>
2024-12-14 13:39:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 55.81 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,45 +1,43 @@
 import java.util.*;
-import java.util.stream.Collectors;
+
+class Truck {
+    int weight;
+    int position;
+
+    Truck(int weight, int position) {
+        this.weight = weight;
+        this.position = position;
+    }
+}
 
 class Solution {
+    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 다리의 길이 bridge_length 만큼 지나가야 다리를 건너는 것.
-        // 다리에 올라갈 수 있는 트럭 무게합은 weight 이하
-
         int answer = 0;
+        
+        Queue<Truck> bridge = new LinkedList<>();
+        
+        int totalWeight = 0;
+        int idx = 0;
 
-        int currentWeight = 0;
-        int time = 0;
+        while (idx < truck_weights.length || !bridge.isEmpty()) {
+            answer++;
 
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < truck_weights.length; i++) {
-            int currentTruck = truck_weights[i];
+            if (!bridge.isEmpty() && bridge.peek().position > bridge_length) {
+                totalWeight -= bridge.poll().weight;
+            }
 
-            while (true) {
-                if (queue.isEmpty()) { // 다리에 트럭이 비어 있는 경우
-                    queue.add(currentTruck);
-                    currentWeight += currentTruck;
-                    time++;
-                    break;
-                } else if (queue.size() == bridge_length) { // 트럭이 다리를 건넌 경우
-                    currentWeight -= queue.poll();
-                } else { // 트럭이 건너고 있는 경우
-                     if (currentWeight + currentTruck <= weight) { // 다리가 무게를 견딜 수 있는 경우
-                        queue.add(currentTruck);
-                        currentWeight += currentTruck;
-                        time++;
-                        break;
-                    } else {
-                        // 다리의 무게가 견디지 못할 경우
-                        queue.add(0);
-                        time++;
-                    }
-                }
+            if (idx < truck_weights.length && totalWeight + truck_weights[idx] <= weight) {
+                bridge.add(new Truck(truck_weights[idx], 1));
+                totalWeight += truck_weights[idx];
+                idx++;
+            }
+
+            for (Truck truck : bridge) {
+                truck.position++;
             }
         }
-
-        answer = time + bridge_length;
-
+        
         return answer;
     }
 }
