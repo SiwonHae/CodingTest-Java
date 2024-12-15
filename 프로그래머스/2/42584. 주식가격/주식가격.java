f37a68df424c0901a106d4e95e9d91ac2cf58f65
JuHyuk Lim <705lo@naver.com>
2024-12-15 13:53:20 +0900

    [level 2] Title: 주식가격, Time: 20.30 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
+        
         for (int i = 0; i < prices.length; i++) {
-            int cnt = 0;
             int price = prices[i];
+            int cnt = 0;
             for (int j = i + 1; j < prices.length; j++) {
-                cnt++;
-                if (price > prices[j]) {
+                if (price <= prices[j]) {
+                    cnt++;
+                } else {
+                    cnt++;
                     break;
                 }
             }
-            
             answer[i] = cnt;
         }
+        
         return answer;
     }
 }
