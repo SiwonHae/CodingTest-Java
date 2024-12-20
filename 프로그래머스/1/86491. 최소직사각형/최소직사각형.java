1f6448cbbd2fba123df3cb2db28d0bc0c1b3545d
JuHyuk Lim <705lo@naver.com>
2024-12-20 16:36:48 +0900

    [level 1] Title: 최소직사각형, Time: 2.02 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,30 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-
-        int[] x = new int[sizes.length];
-        int[] y = new int[sizes.length];
-
+        
+        int maxWidth = 0;
+        int maxHeight = 0;
+        
         for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][0] >= sizes[i][1]) {
-                x[i] = sizes[i][0];
-                y[i] = sizes[i][1];
-            } else {
-                x[i] = sizes[i][1];
-                y[i] = sizes[i][0];
+            int width = sizes[i][0];
+            int height = sizes[i][1];
+            
+            if (width > height) {
+                width = sizes[i][1];
+                height = sizes[i][0];
             }
+            
+            maxWidth = Math.max(maxWidth, width);
+            maxHeight = Math.max(maxHeight, height);
         }
-
-        int w = 0;
-        int h = 0;
-
-        for (int i = 0; i < sizes.length; i++) {
-            w = Math.max(w, x[i]);
-            h = Math.max(h, y[i]);
-        }
-
-        answer = w * h;
-
+        
+        answer = maxWidth * maxHeight;
+        
         return answer;
     }
 }
