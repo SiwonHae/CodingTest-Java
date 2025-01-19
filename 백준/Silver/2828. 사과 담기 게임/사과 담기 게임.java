4a99611ca9cadac66a88b363eb04bf4444819b7b
JuHyuk Lim <705lo@naver.com>
2025-01-19 15:11:03 +0900

    [Silver V] Title: 사과 담기 게임, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -7,36 +7,30 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int bucket = 1;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()); // 바구니의 크기
+
+        int J = Integer.parseInt(br.readLine());
+        int[] pos = new int[J];
+        for (int i = 0; i < J; i++) {
+            pos[i] = Integer.parseInt(br.readLine()) - 1;
+        }
+
         int result = 0;
-        st = new StringTokenizer(br.readLine());
-        int j = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < j; i++) {
-            st = new StringTokenizer(br.readLine());
-            int pos = Integer.parseInt(st.nextToken());
+        int left = 0; // 바구니 왼쪽 끝 위치
+        int right = M - 1; // 바구니 오른쪽 끝 위치
 
-            if (pos >= bucket && pos <= bucket + m - 1) {
-                continue;
-            }
+        for (int i = 0; i < J; i++) {
+            int drop = pos[i];
 
-            if (bucket < pos) {
-                while (true) {
-                    if (pos >= bucket && pos <= bucket + m - 1) {
-                        break;
-                    }
-                    result++;
-                    bucket++; // 바구니의 오른쪽 이동
-                }
-            } else {
-                while (true) {
-                    if (pos >= bucket && pos <= bucket + m - 1) {
-                        break;
-                    }
-                    result++;
-                    bucket--; // 바구니의 왼쪽 이동
-                }
+            if (drop < left) { // 바구니 왼쪽에 떨어지는 경우
+                result += left - drop;
+                right -= left - drop;
+                left = drop;
+            } else if (drop > right) { // 바구니 오른쪽에 떨어지는 경우
+                result += drop - right;
+                left += drop - right;
+                right = drop;
             }
         }
 
