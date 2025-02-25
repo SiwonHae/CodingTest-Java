406915ac1a0ad0c9d31679769744e9e4e4056e80
JuHyuk Lim <705lo@naver.com>
2025-02-25 15:20:19 +0900

    Time: 14 ms (17.21%), Space: 47.1 MB (7.85%) - LeetHub

---
+++ LeetCode/0561-array-partition/0561-array-partition.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        // nums 개를 절반으로 나눈 값 만큼의 pair가 발생하게 된다.
+        // 이 pair의 최대합 구하자.
+
+        Arrays.sort(nums);
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int pair = Math.min(nums[i], nums[i + 1]);
+            result += pair;
+        }
+
+        return result;
+    }
+}
