8436b6fce1b32a4a11a97fc8b82de6fe8c0c991e
JuHyuk Lim <705lo@naver.com>
2025-03-19 16:54:17 +0900

    [Bronze II] Title: 트럭 주차, Time: 96 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -6,36 +6,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-
-        int[] time = new int[102];
-
+        
+        int[] time = new int[101];
+        
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
-
+            
             int arrive = Integer.parseInt(st.nextToken());
-            int leave = Integer.parseInt(st.nextToken());
-
-            for (int j = arrive; j < leave; j++) {
+            int depart = Integer.parseInt(st.nextToken());
+            
+            for (int j = arrive; j < depart; j++) {
                 time[j]++;
             }
         }
-
+        
         int result = 0;
-
-        for (int i = 0; i < time.length; i++) {
+        
+        for (int i = 1; i <= 100; i++) {
             if (time[i] == 1) {
-                result += A;
+                result += A * 1;
             } else if (time[i] == 2) {
                 result += B * 2;
             } else if (time[i] == 3) {
                 result += C * 3;
             }
         }
-
+        
         bw.write(String.valueOf(result));
         bw.flush();
     }
