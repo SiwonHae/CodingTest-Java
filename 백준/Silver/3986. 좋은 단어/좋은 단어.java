923357c04e66a708576d1062a0f6e3950b129b7d
JuHyuk Lim <705lo@naver.com>
2025-03-20 20:24:12 +0900

    [Silver IV] Title: 좋은 단어, Time: 244 ms, Memory: 24156 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -5,36 +5,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int N = Integer.parseInt(br.readLine());
-
+        
         int result = 0;
-
+        
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-
-            Stack<Character> stack = new Stack<>();
-
-            for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-
-                if (stack.isEmpty()) {
-                    stack.push(c);
-                    continue;
-                }
-
-                if (stack.peek() == c) {
-                    stack.pop();
+            Deque<Character> stack = new ArrayDeque<>();
+            String word = br.readLine();
+            
+            for (char c : word.toCharArray()) {
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == c) {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
                 } else {
                     stack.push(c);
                 }
             }
-
+            
             if (stack.isEmpty()) {
                 result++;
             }
         }
-
+        
         bw.write(String.valueOf(result));
         bw.flush();
     }
