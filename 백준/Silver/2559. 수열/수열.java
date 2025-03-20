6bfa033c8ff5155e4c99a91658a6bfd06ad2c7b2
JuHyuk Lim <705lo@naver.com>
2025-03-20 15:40:50 +0900

    [Silver III] Title: 수열, Time: 284 ms, Memory: 26664 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -6,30 +6,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
+        
         st = new StringTokenizer(br.readLine());
-
-        int[] a = new int[N];
-
+        int[] temp = new int[N];
         for (int i = 0; i < N; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            temp[i] = Integer.parseInt(st.nextToken());
         }
-
-        int result = Integer.MIN_VALUE;
-
-        for (int i = 0; i < N - K + 1; i++) {
-            int sum = 0;
-            for (int j = i; j < i + K; j++) {
-                sum += a[j];
-            }
-
-            result = Math.max(result, sum);
+        
+        int[] pSum = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            pSum[i] = pSum[i - 1] + temp[i - 1];
         }
-
-        bw.write(String.valueOf(result));
+        
+        List<Integer> list = new ArrayList<>();
+        int left = 0;
+        int right = K;
+        while (right <= N) {
+            list.add(pSum[right] - pSum[left]);
+            right++;
+            left++;
+        }
+        
+        bw.write(String.valueOf(Collections.max(list)));
         bw.flush();
     }
 }
