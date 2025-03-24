06cc140b2d5c2b8dc2325e663c20ee1dab7ede4b
JuHyuk Lim <705lo@naver.com>
2025-03-24 20:50:28 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -2,57 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static List<Character> vowels = List.of('a', 'e', 'i', 'o', 'u');
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] vowel = {"a", "e", "i", "o", "u"};
-
-        String input = "";
+        String input;
         while (!(input = br.readLine()).equals("end")) {
-
-            boolean isVowel = false;
-            boolean isSeqVowelOrConsonant = false;
-            boolean isSeqSame = false;
-
-            int vowelCnt = 0;
-            int consonantCnt = 0;
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    isVowel = true;
-                    vowelCnt++;
-                    consonantCnt = 0;
-                } else {
-                    consonantCnt++;
-                    vowelCnt = 0;
-                }
-
-                if (vowelCnt >= 3 || consonantCnt >= 3) {
-                    isSeqVowelOrConsonant = true;
-                    break;
-                }
-
-                if (i < input.length() - 1) {
-                    char c2 = input.charAt(i + 1);
-                    if (c == c2) {
-                        if (c != 'e' && c != 'o') {
-                            isSeqSame = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            bw.write("<" + input + "> is ");
-            if (!isVowel || isSeqVowelOrConsonant || isSeqSame) {
-                bw.write("not acceptable.\n");
+            if (isGood(input)) {
+                bw.write("<" + input + ">" + " is acceptable." + "\n");
             } else {
-                bw.write("acceptable.\n");
+                bw.write("<" + input + ">" + " is not acceptable." + "\n");
             }
         }
 
         bw.flush();
+    }
+    
+    public static boolean isGood(String input) {
+        if (!condition1(input)) {
+            return false;
+        }
+        
+        if (!condition2(input)) {
+            return false;
+        }
+        
+        if (!condition3(input)) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public static boolean condition1(String input) {
+        int cnt = 0;
+        
+        for (char c : input.toCharArray()) {
+            if (vowels.contains(c)) {
+                cnt++;
+            }
+        }
+        
+        if (cnt == 0) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public static boolean condition2(String input) {
+        int vowel = 0;
+        int consonant = 0;
+        
+        for (char c : input.toCharArray()) {
+            if (vowels.contains(c)) {
+                vowel++;
+                consonant = 0;
+            } else {
+                consonant++;
+                vowel = 0;
+            }
+            
+            if (vowel == 3 || consonant == 3) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public static boolean condition3(String input) {
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(i + 1) && input.charAt(i) != 'e' && input.charAt(i) != 'o') {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
