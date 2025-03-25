baccdc897fe6d8ae305c8a25c658053db0445364
JuHyuk Lim <705lo@naver.com>
2025-03-25 17:19:47 +0900

    [Silver IV] Title: 수학숙제, Time: 120 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,48 +1,53 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int M = Integer.parseInt(br.readLine());
-        List<BigInteger> list = new ArrayList<>();
-
-        for (int i = 0; i < M; i++) {
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        List<BigInteger> result = new ArrayList<>();
+        
+        for (int i = 0; i < N; i++) {
             String input = br.readLine();
-
-            StringBuilder sb = new StringBuilder();
-            boolean isSeq = false;
-
-            for (int j = 0; j < input.length(); j++) {
-                char c = input.charAt(j);
-
-                if (Character.isDigit(c)) {
-                    sb.append(c);
-                    isSeq = true;
-                } else if (Character.isLetter(c)) {
-                    isSeq = false;
-                }
-
-                if (!isSeq && !sb.toString().equals("")) {
-                    list.add(new BigInteger(sb.toString()));
-                    sb = new StringBuilder();
-                }
-
-                // 마지막 문자인 경우 체크
-                if (j == input.length() - 1 && !sb.toString().equals("")) {
-                    list.add(new BigInteger(sb.toString()));
-                }
+            
+            List<String> nums = parse(input);
+            for (int j = 0; j < nums.size(); j++) {
+                result.add(new BigInteger(nums.get(j)));
             }
         }
-
-        Collections.sort(list);
-        for (BigInteger i : list) {
-            bw.write(i + "\n");
+        
+        Collections.sort(result);
+        
+        for (int i = 0; i < result.size(); i++) {
+            bw.write(result.get(i) + "\n");
         }
-
+        
         bw.flush();
+    }
+    
+    public static List<String> parse(String input) {
+        List<String> list = new ArrayList<>();
+        
+        StringBuilder sb = new StringBuilder();
+        boolean seq = false;
+        
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            } else if (sb.length() != 0) {
+                list.add(sb.toString());
+                sb.setLength(0);
+            }
+        }
+        
+        if (sb.length() != 0) {
+            list.add(sb.toString());
+        }
+        
+        return list;
     }
 }
