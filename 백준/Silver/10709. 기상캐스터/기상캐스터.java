abb9a77eff440d2e52261ae2068cd30463df3f09
JuHyuk Lim <705lo@naver.com>
2025-03-25 17:39:43 +0900

    [Silver V] Title: 기상캐스터, Time: 148 ms, Memory: 17032 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -2,46 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int H, W;
+    static char[][] board;
+    static int[][] result;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int H = Integer.parseInt(st.nextToken());
-        int W = Integer.parseInt(st.nextToken());
-
-        int[][] board = new int[H][W];
-
+        
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        
+        board = new char[H][W];
+        result = new int[H][W];
+        
         for (int i = 0; i < H; i++) {
+            Arrays.fill(result[i], -1);
+            
             String input = br.readLine();
             for (int j = 0; j < W; j++) {
-                if (input.charAt(j) == 'c') {
-                    board[i][j] = 0;
-                } else {
-                    board[i][j] = -1;
+                board[i][j] = input.charAt(j);
+            }
+        }
+        
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if (board[i][j] == 'c' && result[i][j] == -1) {
+                    go(i, j);
                 }
             }
         }
-
+        
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
-                if (board[i][j] == 0) {
-                    for (int k = j + 1; k < W; k++) {
-                        if (board[i][k] != 0) {
-                            board[i][k] = board[i][k - 1] + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                bw.write(board[i][j] + " ");
+                bw.write(result[i][j] + " ");
             }
             bw.write("\n");
         }
 
         bw.flush();
+    }
+    
+    public static void go(int y, int x) {
+        result[y][x] = 0;
+        
+        for (int i = x + 1; i < W; i++) {
+            if (board[y][i] == 'c') {
+                continue;
+            }
+            
+            result[y][i] = result[y][i - 1] + 1;
+        }
     }
 }
