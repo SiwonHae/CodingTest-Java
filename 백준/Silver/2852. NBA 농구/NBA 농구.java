8de632cba962e4949cd291e0213cc99941109ad3
JuHyuk Lim <705lo@naver.com>
2025-03-27 16:08:17 +0900

    [Silver III] Title: NBA 농구, Time: 104 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.java
+++ 백준/Silver/2852. NBA 농구/NBA 농구.java
@@ -3,57 +3,63 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 총 게임 시간 48:00
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int N = Integer.parseInt(br.readLine());
-
-        int score1 = 0;
-        int score2 = 0;
-
-        int lastLeadChangeTime = 0;
-
-        int leadTime1 = 0;
-        int leadTime2 = 0;
-
-        int endTime = 60 * 48;
-
-        StringTokenizer st;
+        
+        int endTime = 48 * 60;
+        
+        int oneTime = 0;
+        int twoTime = 0;
+        
+        int oneScore = 0;
+        int twoScore = 0;
+        
+        int changeTime = 0;
+        
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
             int team = Integer.parseInt(st.nextToken());
-
-            String[] strTime = st.nextToken().split(":");
-            int scoreTime = Integer.parseInt(strTime[0]) * 60 + Integer.parseInt(strTime[1]);
-
-            if (score1 > score2) {
-                leadTime1 += scoreTime - lastLeadChangeTime;
-            } else if (score2 > score1) {
-                leadTime2 += scoreTime - lastLeadChangeTime;
+            String[] time = st.nextToken().split(":");
+            
+            int min = Integer.parseInt(time[0]);
+            int sec = Integer.parseInt(time[1]);
+            
+            int total = min * 60 + sec;
+           
+            if (oneScore > twoScore) {
+                oneTime += total - changeTime;
+            } else if (oneScore < twoScore) {
+                twoTime += total - changeTime;
             }
-
+            
             if (team == 1) {
-                score1++;
+                oneScore++;
             } else {
-                score2++;
+                twoScore++;
             }
-
-            lastLeadChangeTime = scoreTime;
+            
+            changeTime = total;
+            
         }
-
-        if (score1 > score2) {
-            leadTime1 += endTime - lastLeadChangeTime;
-        } else if (score2 > score1) {
-            leadTime2 += endTime - lastLeadChangeTime;
+        
+        if (oneScore > twoScore) {
+            oneTime += endTime - changeTime;
+        } else if (oneScore < twoScore) {
+            twoTime += endTime - changeTime;
         }
+        
+        int oneMin = oneTime / 60;
+        int oneSec = oneTime % 60;
+        int twoMin = twoTime / 60;
+        int twoSec = twoTime % 60;
+        bw.write(String.format("%02d:%02d\n", oneMin, oneSec));
+        bw.write(String.format("%02d:%02d\n", twoMin, twoSec));
 
-        int min1 = leadTime1 / 60;
-        int sec1 = leadTime1 % 60;
-        int min2 = leadTime2 / 60;
-        int sec2 = leadTime2 % 60;
-        bw.write(String.format("%02d:%02d\n", min1, sec1));
-        bw.write(String.format("%02d:%02d\n", min2, sec2));
         bw.flush();
     }
 }
