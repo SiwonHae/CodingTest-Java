eee2c2e885c26a94ab6335868329db21315eaa1a
JuHyuk Lim <705lo@naver.com>
2025-03-30 15:32:15 +0900

    [Gold IV] Title: 오큰수, Time: 844 ms, Memory: 155248 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,36 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
         int[] arr = new int[N];
+        int[] result = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Integer> stack = new Stack<>();
+        // 인덱스를 저장하는 스택
+        Deque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < N; i++) {
             while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                arr[stack.pop()] = arr[i];
+                result[stack.pop()] = arr[i];
             }
 
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            arr[stack.pop()] = -1;
+            result[stack.pop()] = -1;
         }
 
-        for (int i : arr) {
-            bw.write(i + " ");
+        for (int i = 0; i < N; i++) {
+            bw.write(result[i] + " ");
         }
 
         bw.flush();
