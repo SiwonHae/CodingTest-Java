41db7185df400b6e1f7f7f70a8113dd6ecdd9190
JuHyuk Lim <705lo@naver.com>
2025-03-31 14:41:22 +0900

    [Gold V] Title: 치킨 배달, Time: 184 ms, Memory: 15668 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N, M;
     static int[][] board;
-    static List<int[]> house = new ArrayList<>();
-    static List<int[]> chicken = new ArrayList<>();
-    static boolean[] visited;
+    static boolean[] isSelected;
+    static List<Point> chickens = new ArrayList<>();
+    static List<Point> houses = new ArrayList<>();
     static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -18,61 +17,74 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        board = new int[N][N];
+        board = new int[N + 1][N + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 1) {
-                    house.add(new int[] {i, j});
-                } else if (board[i][j] == 2) {
-                    chicken.add(new int[] {i, j});
+
+                if (board[i][j] == 2) {
+                    chickens.add(new Point(i, j));
+                } else if (board[i][j] == 1) {
+                    houses.add(new Point(i, j));
                 }
             }
         }
 
-        visited = new boolean[chicken.size()];
+        // 치킨집 선택 여부를 저장하는 배열
+        // [true, false, true, true] 라면 1번, 3번, 4번 치킨집이 선택되었다는 것.
+        isSelected = new boolean[chickens.size()];
 
-        // 1. 치킨집 M개를 고른다.
-        // 2. 최소 도시의 치킨 거리를 구한다.
-
-        backtracking(0, 0);
+        combination(0, 0);
 
         bw.write(String.valueOf(result));
         bw.flush();
     }
 
-    public static void backtracking(int depth, int start) {
+    public static void combination(int start, int depth) {
         if (depth == M) {
-            int sum = 0;
-
-            for (int i = 0; i < house.size(); i++) {
-                int[] h = house.get(i);
-                int hy = h[0];
-                int hx = h[1];
-
-                int min = Integer.MAX_VALUE;
-
-                for (int j = 0; j < chicken.size(); j++) {
-                    if (visited[j]) {
-                        int[] s = chicken.get(j);
-                        int sy = s[0];
-                        int sx = s[1];
-                        min = Math.min(min, Math.abs(hy - sy) + Math.abs(hx - sx));
-                    }
-                }
-                sum += min;
-            }
-
-            result = Math.min(result, sum);
+            dfs();
             return;
         }
 
-        for (int i = start; i < chicken.size(); i++) {
-            visited[i] = true;
-            backtracking(depth + 1, i + 1);
-            visited[i] = false;
+        for (int i = start; i < chickens.size(); i++) {
+            isSelected[i] = true;
+            combination(i + 1, depth + 1);
+            isSelected[i] = false;
+        }
+    }
+
+    public static void dfs() {
+        int sumDistance = 0;
+
+        for (Point house : houses) {
+            int distance = Integer.MAX_VALUE;
+
+            for (int i = 0; i < chickens.size(); i++) {
+                if (isSelected[i]) {
+                    Point chicken = chickens.get(i);
+                    distance = Math.min(distance, calcDistance(house.y, house.x, chicken.y, chicken.x));
+                }
+            }
+
+            sumDistance += distance;
+        }
+
+        result = Math.min(result, sumDistance);
+    }
+
+    public static int calcDistance(int y1, int x1, int y2, int x2) {
+        return Math.abs(y1 - y2) + Math.abs(x1 - x2);
+    }
+
+    static class Point {
+        int y;
+        int x;
+
+        Point(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
     }
 }
