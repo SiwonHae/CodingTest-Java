7c2a7065abe3cc0017597b9df3bb57f2f74eb2f4
JuHyuk Lim <705lo@naver.com>
2025-04-30 20:14:49 +0900

    [Gold II] Title: 컵라면, Time: 692 ms, Memory: 71824 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,51 +9,43 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         List<Problem> list = new ArrayList<>();
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        boolean[] visited = new boolean[N + 1];
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int deadline = Integer.parseInt(st.nextToken());
-            int cnt = Integer.parseInt(st.nextToken());
-            list.add(new Problem(deadline, cnt));
+            int value = Integer.parseInt(st.nextToken());
+            list.add(new Problem(deadline, value));
         }
 
         Collections.sort(list, (o1, o2) -> {
-            if (o1.deadline == o2.deadline) {
-                return o2.cnt - o1.cnt;
-            }
-
             return o1.deadline - o2.deadline;
         });
 
-        int result = 0;
-
-        for (int i = 0; i < N; i++) {
-            int deadline = list.get(i).deadline;
-            int cnt = list.get(i).cnt;
-
-            result += cnt;
-
-            pq.add(cnt);
-            if (pq.size() > deadline) {
-                result -= pq.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (Problem p : list) {
+            if (pq.size() < p.deadline) {
+                pq.offer(p.value);
+            } else if (!pq.isEmpty() && pq.peek() < p.value) {
+                pq.poll();
+                pq.offer(p.value);
             }
+        }
+
+        int result = 0;
+        while (!pq.isEmpty()) {
+            result += pq.poll();
         }
 
         bw.write(String.valueOf(result));
         bw.flush();
     }
-}
 
-class Problem {
-    int deadline;
-    int cnt;
+    static class Problem {
+        int deadline;
+        int value;
 
-    public Problem(int deadline, int cnt) {
-        this.deadline = deadline;
-        this.cnt = cnt;
+        public Problem(int deadline, int value) {
+            this.deadline = deadline;
+            this.value = value;
+        }
     }
 }
