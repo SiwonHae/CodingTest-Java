7453b9a2daf901633543207bffcd29c5e11dc6a4
JuHyuk Lim <705lo@naver.com>
2025-05-16 12:54:24 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] cost = new int[N + 1][3];
+        int[][] dp = new int[N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[1][0] = cost[1][0];
+        dp[1][1] = cost[1][1];
+        dp[1][2] = cost[1][2];
+
+        for (int i = 2; i <= N; i++) {
+            // i번째 집을 빨강으로 칠하려면 i-1번쨰 집이 초록이거나 파랑이여야 한다.
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        }
+
+        int result = Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2]));
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
