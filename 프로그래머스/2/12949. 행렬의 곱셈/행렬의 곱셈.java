5870aaf72818ab1fb4dd08278fb75376ef1a0904
JuHyuk Lim <705lo@naver.com>
2025-06-10 16:46:53 +0900

    [level 2] Title: 행렬의 곱셈, Time: 15.83 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,24 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
-
-        // 2 x 2 행렬과 2 x 3 행렬의 곱셈은 2 x 3 행렬이 된다.
-        // A행렬의 열의 개수와 B행렬의 행의 개수가 같아야 곱셈이 된다.
-        // arr1.length = 2
-        // arr1[0].length = 2
-        // arr2.length = 2
-        // arr2[0].length = 3
-
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2[0].length; j++) {
-                int tmp = 0;
-                for (int k = 0; k < arr2.length; k++) {
-                    tmp += arr1[i][k] * arr2[k][j];
+        // 3x2 * 2x2 = 3x2
+        
+        int r1 = arr1.length;
+        int c1 = arr1[0].length;
+        int r2 = arr2.length;
+        int c2 = arr2[0].length;
+        
+        int[][] answer = new int[r1][c2];
+        
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-                answer[i][j] = tmp;
             }
         }
-
+        
+        
         return answer;
     }
 }
