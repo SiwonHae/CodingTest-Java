50c99cb182b045c34dd4649f89522a2b59e91757
JuHyuk Lim <705lo@naver.com>
2025-06-10 17:06:12 +0900

    [level 1] Title: 실패율, Time: 184.12 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,37 +1,28 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-
-        int[] last = new int[N + 1]; // 각 단계에 도전 중인 플레이어 수(인덱스 : 단계 번호)
-        int[] total = new int[N + 1]; // 스테이지에 도달한 플에이어의 수(인덱스 : 단계 번호)
-        for (int i = 0; i < stages.length; i++) {
-            int cnt = 0;
-            last[stages[i] - 1]++;
-            while (cnt != stages[i]) {
-                total[cnt]++;
-                cnt++;
-            }
-        }
-
         Map<Integer, Double> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            // 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0 으로 정의한다.
-            if (last[i] == 0) {
-                map.put(i, 0.0);
-            } else {
-                map.put(i, (double) last[i] / total[i]);
+        
+        int people = stages.length;
+        
+        for (int i = 1; i <= N; i++) {
+            int failPeople = 0;
+            
+            for (int j = 0; j < stages.length; j++) {        
+                if (i == stages[j]) {
+                    failPeople++;
+                }
             }
+
+            map.put(i, (people == 0) ? 0.0 : (double) failPeople / people);
+            people -= failPeople;
         }
-
-        List<Integer> keySet = new ArrayList<>(map.keySet());
-        keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
-
-        for (int i = 0; i < keySet.size(); i++) {
-            answer[i] = keySet.get(i) + 1;
-        }
-
-        return answer;
+        
+        return map.entrySet().stream()
+            .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
+            .mapToInt(HashMap.Entry::getKey)
+            .toArray();
     }
 }
