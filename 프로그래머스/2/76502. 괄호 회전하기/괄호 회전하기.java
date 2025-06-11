dcf344a75c0fd29639a55645a4d9570a9d9cfc30
JuHyuk Lim <705lo@naver.com>
2025-06-11 15:08:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 68.39 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,51 +3,60 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-
-        List<String> list = new ArrayList<>();
-        list.add(s);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            char c = s.charAt(0);
-            s = s.substring(1, s.length());
-            s += c;
-            list.add(s);
-        }
-
-        Stack<Character> stack = new Stack<>();
-        for (int j = 0; j < list.size(); j++) {
-            String str = list.get(j);
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-
-                if (stack.isEmpty()) {
-                    stack.push(c);
-                } else if (c == '(') {
-                    stack.push(c);
-                } else if (c == '{') {
-                    stack.push(c);
-                } else if (c == '[') {
-                    stack.push(c);
-                } else if (c == ')') {
-                    if (stack.peek() == '(') {
-                        stack.pop();
-                    }
-                } else if (c == '}') {
-                    if (stack.peek() == '{') {
-                        stack.pop();
-                    }
-                } else if (c == ']') {
-                    if (stack.peek() == '[') {
-                        stack.pop();
-                    }
-                }
-            }
-            if (stack.isEmpty()) {
+        
+        for (int i = 0; i < s.length(); i++) {
+            // 올바른 괄호 문자열 체크
+            if (check(s)) {
                 answer++;
             }
-            stack.clear();
+            
+            // 문자열 왼쪽으로 i 만큼 회전
+            s = rotate(s, i);
         }
-
+        
         return answer;
+    }
+    
+    public boolean check(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                stack.push(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public String rotate(String s, int x) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+        }
+        
+        sb.append(s.charAt(0));
+        
+        return sb.toString();
     }
 }
