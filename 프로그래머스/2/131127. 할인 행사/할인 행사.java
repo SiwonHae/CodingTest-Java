fe6c866fcd8381fe48ba696579b624289445686d
JuHyuk Lim <705lo@naver.com>
2025-06-13 16:03:48 +0900

    [level 2] Title: 할인 행사, Time: 75.38 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,38 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-
+        
         Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
-
-        for (int i = 0; i < discount.length; i++) {
-            boolean isBuy = true;
-            Map<String, Integer> currentMap = new HashMap<>();
-            for (int j = 0; j < want.length; j++) {
-                currentMap.put(want[j], 0);
+        
+        
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> discountMap = new HashMap<>();
+            
+            for (int j = i; j < i + 10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
             }
             
-            for (int j = i; j < discount.length; j++) { // 수정된 부분
-                currentMap.put(discount[j], currentMap.getOrDefault(discount[j], 0) + 1);
-                if (j - i == 9) // 10일 동안의 할인 정보를 추적합니다.
-                    break;
-            }
-
-            for (int j = 0; j < want.length; j++) {
-                if (wantMap.get(want[j]) > currentMap.get(want[j])) {
-                    isBuy = false;
+            boolean flag = true;
+            for (String name : want) {
+                if (wantMap.get(name) > discountMap.getOrDefault(name, 0)) {
+                    flag = false;
                     break;
                 }
             }
-
-            if (isBuy) {
+            
+            if (flag) {
                 answer++;
             }
-
         }
-
+        
         return answer;
     }
 }
