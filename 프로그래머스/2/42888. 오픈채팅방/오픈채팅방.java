2d948bde160d73e255043d2e2e89469408eda5fb
JuHyuk Lim <705lo@naver.com>
2025-06-16 14:14:57 +0900

    [level 2] Title: 오픈채팅방, Time: 142.21 ms, Memory: 192 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,48 +1,35 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String[] solution(String[] record) {
-
-        // "명령 아이디 닉네임"
-
-        List<String> idList = new ArrayList<>();
-        List<String> cmdList = new ArrayList<>();
-        Map<String, String> infoMap = new HashMap<>(); // 아이디, 닉네임
-
-        for (int i = 0; i < record.length; i++) {
-            String[] log = record[i].split(" ");
-
-            String cmd = log[0];
-            String id = log[1];
-            String nickname = "";
-
-            if (cmd.equals("Enter")) {
-                nickname = log[2];
-                idList.add(id);
-                cmdList.add(cmd);
-                infoMap.put(id, nickname);
-            } else if (cmd.equals("Leave")) {
-                idList.add(id);
-                cmdList.add(cmd);
-            } else if (cmd.equals("Change")) {
-                nickname = log[2];
-                infoMap.put(id, nickname);
+        Map<String, String> map = new HashMap<>(); // 유저 아이디와 닉네임
+        
+        for (String cmds : record) {
+            String[] cmd = cmds.split(" ");
+            String action = cmd[0];
+            
+            if (action.equals("Enter") || action.equals("Change")) {
+                String userId = cmd[1];
+                String nickname = cmd[2];
+                map.put(userId, nickname);
             }
         }
-
-        String[] answer = new String[idList.size()];
-
-        for (int i = 0; i < idList.size(); i++) {
-            String id = idList.get(i);
-            String nickname = infoMap.get(id);
-
-            if (cmdList.get(i).equals("Enter")) {
-                answer[i] = nickname + "님이 들어왔습니다.";
-            } else if (cmdList.get(i).equals("Leave")) {
-                answer[i] = nickname + "님이 나갔습니다.";
+        
+        List<String> result = new ArrayList<>();
+        
+        for (String cmds : record) {
+            String[] cmd = cmds.split(" ");
+            String action = cmd[0];
+            String userId = cmd[1];
+            
+            if (action.equals("Enter")) {
+                result.add(map.get(userId) + "님이 들어왔습니다.");
+            } else if (action.equals("Leave")) {
+                result.add(map.get(userId) + "님이 나갔습니다.");
             }
         }
-
-        return answer;
+        
+        return result.toArray(new String[0]); 
     }
 }
