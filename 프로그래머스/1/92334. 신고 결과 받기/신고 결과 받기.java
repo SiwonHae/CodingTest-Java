18fde5a24d3f6ed42b11b8d1e5e7bd6321624c72
JuHyuk Lim <705lo@naver.com>
2025-06-16 15:36:39 +0900

    [level 1] Title: 신고 결과 받기, Time: 226.96 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,41 +1,52 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-
-        Map<String, Integer> idMap = new HashMap<>();
-        // 이름 : 인덱스 번호
-        // muzi : 0
-        // frodo : 1
-        // apeach : 2
-        // neo : 3
-
-        Map<String, HashSet<String>> reportInfo = new HashMap<>();
-        // 신고 당한 이름 : 신고한 사람 이름
-        // muzi : {appeach} -> 무지를 신고한 사람이 appeach임.
-
-        for (int i = 0; i < id_list.length; i++) {
-            String name = id_list[i];
-            idMap.put(name, i);
-            reportInfo.put(name, new HashSet<>());
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> cntMap = new HashMap<>();
+        
+        for (String id : id_list) {
+            reportMap.put(id, new HashSet<>());
         }
-
-        for (int i = 0; i < report.length; i++) {
-            String reporter = report[i].split(" ")[0];
-            String target = report[i].split(" ")[1];
-            reportInfo.get(target).add(reporter);
+        
+        for (String rep : report) {
+            String[] splitReport = rep.split(" ");
+            String from = splitReport[0];
+            String to = splitReport[1];
+            
+            reportMap.get(from).add(to);
         }
-
-        for (String key : reportInfo.keySet()) {
-            if (reportInfo.get(key).size() >= k) {
-                for (String id : reportInfo.get(key)) {
-                    answer[idMap.get(id)]++;
-                }
+        
+        for (Set<String> reported : reportMap.values()) {
+            for (String id : reported) {
+                cntMap.put(id, cntMap.getOrDefault(id, 0) + 1);
             }
         }
-
-
+        
+        Set<String> blocked = new HashSet<>();
+        for (String id : cntMap.keySet()) {
+            if (cntMap.get(id) >= k) {
+                blocked.add(id);
+            }
+        }
+        
+        int[] answer = new int[id_list.length];
+        
+        for (int i = 0; i < id_list.length; i++) {
+            String id = id_list[i];
+            Set<String> reported = reportMap.get(id);
+            
+            int mailCnt = 0;
+            for (String userId : reported) {
+                if (blocked.contains(userId)) {
+                    mailCnt++;
+                }
+            }
+            
+            answer[i] = mailCnt;
+        }
+        
         return answer;
     }
 }
