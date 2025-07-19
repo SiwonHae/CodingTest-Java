6fc6b0b1df6940359f46eceb60073eb397d7b7d6
JuHyuk Lim <705lo@naver.com>
2025-07-19 14:33:16 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 24012 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -5,27 +5,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int k = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        int K = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < k; i++) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < K; i++) {
             int num = Integer.parseInt(br.readLine());
 
-            if (num != 0) {
-                stack.add(num);
-            } else {
+            if (num == 0) {
                 stack.pop();
+            } else {
+                stack.push(num);
             }
         }
 
-        int sum = 0;
+        int result = 0;
         while (!stack.isEmpty()) {
-            sum += stack.pop();
+            result += stack.pop();
         }
-        
-        bw.write(String.valueOf(sum));
+
+        bw.write(String.valueOf(result));
         bw.flush();
     }
 }
