12e719b7f348a7850d8aa1d37dd836bc408d0cb3
JuHyuk Lim <705lo@naver.com>
2025-08-30 15:57:37 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,32 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int N = Integer.parseInt(br.readLine());
-        int[][] cost = new int[N + 1][3];
-        int[][] dp = new int[N + 1][3];
-
+        
+        int[][] costs = new int[N + 1][3];
+        
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-
-        dp[1][0] = cost[1][0];
-        dp[1][1] = cost[1][1];
-        dp[1][2] = cost[1][2];
-
+        
+        int[][] dp = new int[N + 1][3];
+        dp[1][0] = costs[1][0];
+        dp[1][1] = costs[1][1];
+        dp[1][2] = costs[1][2];
+        
         for (int i = 2; i <= N; i++) {
-            // i번째 집을 빨강으로 칠하려면 i-1번쨰 집이 초록이거나 파랑이여야 한다.
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
-
-        int result = Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2]));
-
-        bw.write(String.valueOf(result));
+        
+        bw.write(String.valueOf(Collections.min(Arrays.asList(dp[N][0], dp[N][1], dp[N][2]))));
         bw.flush();
     }
 }
