755f667c041298b1357c1586b07ca0c81f6ce368
JuHyuk Lim <705lo@naver.com>
2025-09-04 22:10:01 +0900

    [Silver II] Title: N과 M (11), Time: 320 ms, Memory: 33696 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] seq;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        seq = new int[N];
+        arr = new int[M];
+        visited = new boolean[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(seq);
+        
+        backtrack(0);
+        
+        bw.write(sb.toString());
+        bw.flush();
+    }
+    
+    public static void backtrack(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        int tmp = 0;
+        
+        for (int i = 0; i < N; i++) {
+            if (tmp == seq[i]) {
+                continue;
+            }
+            
+            arr[depth] = seq[i];
+            tmp = seq[i];
+            backtrack(depth + 1);
+        }
+    }
+}
