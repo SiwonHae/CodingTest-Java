3abe53250f12ec09cbb15277d9388d1084092c31
JuHyuk Lim <705lo@naver.com>
2025-09-06 21:41:03 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -5,42 +5,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-
+        
+        int N = Integer.parseInt(br.readLine());
+        
         int result = 0;
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            boolean[] prime = new boolean[num + 1];
-            isPrime(num, prime);
-
-            if (prime[num]) {
+            
+            if (isPrime(num)) {
                 result++;
             }
         }
-
+        
         bw.write(String.valueOf(result));
         bw.flush();
     }
-
-    public static boolean[] isPrime(int num, boolean[] prime) {
-
-        Arrays.fill(prime, true);
-
-        prime[0] = false;
-        prime[1] = false;
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (prime[i]) {
-                for (int j = i * i; j <= num; j+= i) {
-                    prime[j] = false;
-                }
+    
+    public static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+        
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
             }
         }
-
-        return prime;
+        
+        return true;
     }
 }
