f017eb89bff7350d8c3c1b5d55a4b1ab5dc279c0
JuHyuk Lim <705lo@naver.com>
2025-09-08 22:58:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,71 +2,88 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N;
     static int[][] board;
     static boolean[][] visited;
+    static List<Integer> result = new ArrayList<>();
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
-    static int total = 0;
     static int cnt = 1;
-    static List<Integer> cntList = new ArrayList<>();
-
+    static int totalCnt;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         N = Integer.parseInt(br.readLine());
+        
         board = new int[N][N];
         visited = new boolean[N][N];
-
+        
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                char c = s.charAt(j);
-                board[i][j] = c - '0';
+                board[i][j] = input.charAt(j) - '0';
             }
         }
-
+        
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 1 && !visited[i][j]) {
-                    dfs(i, j);
-                    cntList.add(cnt);
-                    cnt = 1;
-                    total++;
+                if (board[i][j] == 0 || visited[i][j]) {
+                    continue;
+                }
+                bfs(i, j);
+                result.add(cnt);
+                cnt = 1;
+                totalCnt++;
+            }
+        }
+        
+        bw.write(String.valueOf(totalCnt));
+        bw.newLine();
+        Collections.sort(result);
+        for (int i = 0; i < result.size(); i++) {
+            bw.write(String.valueOf(result.get(i)));
+            bw.newLine();
+        }
+        
+        bw.flush();
+    }
+    
+    public static void bfs(int startY, int startX) {
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(startY, startX));
+        visited[startY][startX] = true;
+        
+        while (!queue.isEmpty()) {
+            Point current = queue.poll();
+            int y = current.y;
+            int x = current.x;
+            
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                
+                if (ny < 0 || ny >= N || nx < 0 || nx >= N) {
+                    continue;
+                }
+                
+                if (!visited[ny][nx] && board[ny][nx] == 1) {
+                    cnt++;
+                    queue.offer(new Point(ny, nx));
+                    visited[ny][nx] = true;
                 }
             }
         }
-
-        bw.write(total + "\n");
-        Collections.sort(cntList);
-        for (int i : cntList) {
-            bw.write(i + "\n");
-        }
-
-        bw.flush();
     }
-
-    public static void dfs(int y, int x) {
-        if (visited[y][x]) {
-            return;
-        }
-
-        visited[y][x] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny < 0 || ny >= N || nx < 0 || nx >= N) {
-                continue;
-            }
-
-            if (board[ny][nx] == 1 && !visited[ny][nx]) {
-                dfs(ny, nx);
-                cnt++;
-            }
+    
+    static class Point {
+        int y;
+        int x;
+        
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
     }
 }
