648f82c5b28e829621213b5f4e5cd4d418a3a03a
JuHyuk Lim <705lo@naver.com>
2025-09-10 20:23:58 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 572 ms, Memory: 62268 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,57 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int N;
-    static List<List<Integer>> tree;
-    static boolean[] visited;
+    static List<List<Integer>> adjList;
     static int[] parent;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         N = Integer.parseInt(br.readLine());
-
-        tree = new ArrayList<>(); // adj list
+        
+        adjList = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            tree.add(new ArrayList<>());
+            adjList.add(new ArrayList<>());
         }
-
-        visited = new boolean[N + 1];
+        
         parent = new int[N + 1];
-
+        
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            tree.get(a).add(b);
-            tree.get(b).add(a);
+            
+            adjList.get(a).add(b);
+            adjList.get(b).add(a);
         }
-
-        dfs(1);
-
+        
+        bfs(1);
+        
         for (int i = 2; i <= N; i++) {
             bw.write(parent[i] + "\n");
         }
-
+        
         bw.flush();
     }
-
-    public static void dfs(int node) {
-        if (visited[node]) {
-            return;
-        }
-
-        visited[node] = true;
-
-        for (int n : tree.get(node)) {
-            if (!visited[n]) {
-                parent[n] = node;
-                dfs(n);
+    
+    public static void bfs(int start) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(start);
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            
+            for (int next : adjList.get(current)) {
+                if (parent[current] == next) {
+                    continue;
+                }
+                
+                parent[next] = current;
+                queue.offer(next);
             }
         }
-
+        
     }
 }
