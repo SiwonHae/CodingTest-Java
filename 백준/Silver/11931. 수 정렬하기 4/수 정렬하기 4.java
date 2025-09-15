011a73d2a4d627cbb355560822725049ebc0f1c7
JuHyuk Lim <705lo@naver.com>
2025-09-15 21:46:24 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1468 ms, Memory: 138460 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        
+        Collections.sort(list, Collections.reverseOrder());
+        
+        for (int x : list) {
+            bw.write(x + "\n");
+        }
+        
+        bw.flush();
+    }
+}
