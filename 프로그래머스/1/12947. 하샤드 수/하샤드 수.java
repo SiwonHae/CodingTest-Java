d9addc203f7f3c76c2ff04b93cca05d343145c35
JuHyuk Lim <705lo@naver.com>
2025-09-28 18:58:46 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,15 +2,14 @@ class Solution {
     public boolean solution(int x) {
         boolean answer = true;
         
-        int num = x;
-
-        int sum = 0;        
-        while (num > 0) {            
-            sum += num % 10;
-            num /= 10;
+        int original = x;
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
         }
         
-        if (x % sum != 0) {
+        if (original % sum != 0) {
             answer = false;
         }
         
