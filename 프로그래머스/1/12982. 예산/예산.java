819138936a55c8c3a1dca6ff5255b558ed5d14c8
JuHyuk Lim <705lo@naver.com>
2025-10-03 16:01:23 +0900

    [level 1] Title: 예산, Time: 0.46 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,29 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-
+        
         Arrays.sort(d);
-
-        int index = 0;
-        int sum = 0;
-        for (int i = 0; i < d.length; i++) {
-            sum += d[i];
-            if (sum > budget) {
-                index = i;
-                break;
-            } else if (sum == budget) {
-                index = i + 1;
-                break;
-            } else if (i == d.length - 1) {
-                index = d.length;
+        for (int x : d) {
+            if (budget - x < 0) {
                 break;
             }
+            
+            budget -= x;
+            answer++;
         }
-
-        answer = index;
-
+        
         return answer;
     }
 }
