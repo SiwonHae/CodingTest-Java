e0e699d9278797e6e8c0a9db33d02f4e9ede0c9d
JuHyuk Lim <705lo@naver.com>
2025-10-07 14:01:23 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.62 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,33 +1,17 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-
-        // food[0]은 1로 고정(준비한 물의 양)
-        // food[1] 부터 칼로리가 낮은 순으로 음식의 개수
-        String front = "";
-        String back = "";
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = 1; i < food.length; i++) {
-            int cnt = 0;
-            if (food[i] % 2 == 0) {
-                while (cnt != (food[i] / 2)) {
-                    front += i;
-                    cnt++;
-                }
-            } else {
-                while (cnt != (food[i] / 2)) {
-                    front += i;
-                    cnt++;
-                }
+            int cnt = food[i] / 2;
+            for (int j = 0; j < cnt; j++) {
+                sb.append(i);
             }
         }
-        answer += front;
-        answer += "0";
-
-        StringBuffer sb = new StringBuffer(front);
-        back = sb.reverse().toString();
-
-        answer += back;
-
-        return answer;
+        
+        String back = sb.reverse().toString();
+        String front = sb.reverse().append("0").toString();
+        
+        return front + back;
     }
 }
