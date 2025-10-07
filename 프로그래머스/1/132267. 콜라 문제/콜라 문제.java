b8528946ea13eedac2c4fdde35b111ec9f46b0b7
JuHyuk Lim <705lo@naver.com>
2025-10-07 14:15:49 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-
-        // a개 갖다주면 b개 줌.
-
+        
         while (n >= a) {
-            n = n - a; // a개 가져다 주면
-            n += b; // b개를 줌
-            answer += b;
+            int get = (n / a) * b;
+            int current = n % a;
+            
+            answer += get;
+            n = get + current;
         }
-
+        
         return answer;
     }
 }
