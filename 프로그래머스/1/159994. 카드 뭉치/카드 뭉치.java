0bd790c1c0ecd68a4ef04b6526a2e8243a43073e
JuHyuk Lim <705lo@naver.com>
2025-10-08 17:10:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.06 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,24 +1,15 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Queue<String> queue1 = new ArrayDeque<>();
-        Queue<String> queue2 = new ArrayDeque<>();
+        int idx1 = 0;
+        int idx2 = 0;
         
-        for (String word : cards1) {
-            queue1.offer(word);
-        }
-        
-        for (String word : cards2) {
-            queue2.offer(word);
-        }
-        
-        for (String target : goal) {
-            if (!queue1.isEmpty() && queue1.peek().equals(target)) {
-                queue1.poll();
-            } else if (!queue2.isEmpty() && queue2.peek().equals(target)) {
-                queue2.poll();
+        for (int i = 0; i < goal.length; i++) {
+            String s = goal[i];
+            
+            if (idx1 < cards1.length && s.equals(cards1[idx1])) {
+                idx1++;
+            } else if (idx2 < cards2.length && s.equals(cards2[idx2])) {
+                idx2++;
             } else {
                 return "No";
             }
