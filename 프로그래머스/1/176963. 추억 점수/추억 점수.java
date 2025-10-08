ca53266465909b8ad013be343ffb002ac4094cac
JuHyuk Lim <705lo@naver.com>
2025-10-08 17:16:58 +0900

    [level 1] Title: 추억 점수, Time: 2.22 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,19 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        
         for (int i = 0; i < photo.length; i++) {
             int sum = 0;
             for (int j = 0; j < photo[i].length; j++) {
-                for (int k = 0; k < name.length; k++) {
-                    if (photo[i][j].equals(name[k])) {
-                        sum += yearning[k];
-                    }
+                String person = photo[i][j];
+                
+                if (map.containsKey(person)) {
+                    sum += map.get(person);
                 }
             }
             answer[i] = sum;
         }
-
+        
         return answer;
     }
 }
