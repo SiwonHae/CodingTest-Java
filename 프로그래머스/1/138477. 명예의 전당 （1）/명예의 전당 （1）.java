4edb0cdde131c36a36f3bfdb44cac419a76db12e
JuHyuk Lim <705lo@naver.com>
2025-10-08 17:06:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.26 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,22 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-
-        List<Integer> honor = new ArrayList<>();
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         for (int i = 0; i < score.length; i++) {
-            if (honor.size() < k) {
-                honor.add(score[i]);
-            } else {
-                int min = Collections.min(honor);
-                if (min < score[i]) {
-                    honor.remove(Integer.valueOf(min));
-                    honor.add(score[i]);
-                }
+            pq.offer(score[i]);
+            
+            if (pq.size() > k) {
+                pq.poll();
             }
-
-            answer[i] = Collections.min(honor);
+            
+            answer[i] = pq.peek();
         }
-
+        
         return answer;
     }
 }
