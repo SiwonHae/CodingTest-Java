95cbf97fb238062cccf54570d87738d7a77545f2
JuHyuk Lim <705lo@naver.com>
2025-10-11 15:38:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 112.49 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,33 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= number; i++) {
-            int cnt = 0; // 약수의 개수
-
-            for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) {
-                    cnt++;
-                } else if (i % j == 0) {
-                    cnt += 2;
-                }
-            }
-
-            if (cnt > limit) {
-                list.add(power);
+        
+        // 약수의 개수가 limit을 넘으면 power의 공격력을 가진다.
+        for (int knight = 1; knight <= number; knight++) {
+            int n = solve(knight);
+            
+            if (n > limit) {
+                answer += power;
             } else {
-                list.add(cnt);
+                answer += n;
             }
         }
-
-        for (int i : list) {
-            answer += i;
-        }
-
+        
         return answer;
+    }
+    
+    public int solve(int n) {
+        int sum = 0;
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) {
+                sum++;
+            } else if (n % i == 0) {
+                sum += 2;
+            }
+        }
+        
+        return sum;
     }
 }
