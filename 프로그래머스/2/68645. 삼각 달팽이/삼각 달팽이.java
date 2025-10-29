ac3187a239d434e1e93426a497ad3cba1769dbf9
Juhyuk Lim <705lo@naver.com>
2025-10-29 21:37:16 +0900

    [level 2] Title: 삼각 달팽이, Time: 11.93 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,55 +1,62 @@
 class Solution {
-
-    // x축 변화량 (아래, 오른쪽, 왼쪽 위)
-    private static final int[] dx = {0, 1, -1};
-    // y축 변화량 (아래, 오른쪽, 왼쪽 위)
-    private static final int[] dy = {1, 0, -1};
-
     public int[] solution(int n) {
-        // 밑변의 길이와 높이가 n인 삼각형
-        // n = 6
-        // 1
-        // 2  15
-        // 3  16  14
-        // 4  17  21  13
-        // 5  18  19  20  12
-        // 6   7   8   9  10  11
-
-        int[][] board = new int[n][n];
-
-        int x = 0;
+        int num = 0;
+        for (int i = 1; i <= n; i++) {
+            num += i;
+        }
+        
+        int[][] arr = new int[n][];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new int[i + 1];
+        }
+        
         int y = 0;
-        int d = 0; // dx, dy를 위한 방향 변수
-        int v = 1;
-
-        while (true) {
-            board[y][x] = v++;
-
-            int nx = x + dx[d]; // 다음 x 위치
-            int ny = y + dy[d]; // 다음 y 위치
-
-            if (nx == n || ny == n || nx == -1 || ny == -1 || board[ny][nx] != 0) {
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-
-                if (nx == n || ny == n || nx == -1 || ny == -1 || board[ny][nx] != 0) {
+        int x = 0;
+        int cur = 1;
+        while (cur <= num) {
+            // 아래로 채우기
+            while (y < n) {
+                if (arr[y][x] != 0) {
                     break;
                 }
+                arr[y][x] = cur++;
+                y++;
             }
-
-            x = nx;
-            y = ny;
+            y--;
+            x++;
+            
+            // 오른쪽으로 채우기
+            while (x < arr[y].length) {
+                if (arr[y][x] != 0) {
+                    break;
+                }
+                arr[y][x] = cur++;
+                x++;
+            }
+            x -= 2;
+            y--;
+            
+            // 왼쪽 위 대각선으로 채우기
+            while (y >= 0 && x >= 0) {
+                if (arr[y][x] != 0) {
+                    break;
+                }
+                arr[y][x] = cur++;
+                y--;
+                x--;
+            }
+            y += 2;
+            x++;
         }
-
-        int[] answer = new int[v - 1];
+        
+        int[] answer = new int[num];
         int idx = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                answer[idx++] = board[i][j];
+            for (int j = 0; j < arr[i].length; j++) {
+                answer[idx++] = arr[i][j];
             }
         }
-
+        
         return answer;
     }
 }
