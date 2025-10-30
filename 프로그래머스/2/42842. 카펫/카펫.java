c1f7aa7c487c4fed35f082a96842358f00ccc32c
Juhyuk Lim <705lo@naver.com>
2025-10-30 21:40:45 +0900

    [level 2] Title: 카펫, Time: 10.60 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,29 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-
-        int sum = brown + yellow;
-
-        // 최소 가로와 세로는 3 이상임
-
-        for (int i = 3; i <= sum / 2; i++) {
-            for (int j = 3; j <= sum / 2; j++) {
-                if (i * j == sum) {
-                    // 세로가 i, 가로가 j 임.
-                    // 이때 노란색이 다 들어가는지 검증 필요함.
-
-                    int width = (j - 2) * 2; // 가로
-                    int height = i * 2; // 세로
-
-                    if (sum - (width + height) == yellow) {
-                        answer[1] = i;
-                        answer[0] = j;
-                        return answer;
-                    }
+        for (int i = 3; i <= 5000; i++) {
+            for (int j = 3; j <= i; j++) {
+                int brownCnt = i * 2 + (j - 2) * 2;
+                int yelloCnt = i * j - brownCnt;
+                
+                if (brown == brownCnt && yellow == yelloCnt) {
+                    return new int[] {i, j};
                 }
             }
         }
-
-        return answer;
+        return null;
     }
 }
