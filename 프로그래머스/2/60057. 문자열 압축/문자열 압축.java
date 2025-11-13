feeecaeb42eb0cf0917c9a77164ef6a541a79774
Juhyuk Lim <705lo@naver.com>
2025-11-13 21:50:43 +0900

    [level 2] Title: 문자열 압축, Time: 9.85 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,55 +1,39 @@
-import java.util.*;
-
 class Solution {
+    public int solution(String s) {
+        int answer = s.length();
 
-    private List<String> splitToken(String source, int length) {
-        List<String> tokens = new ArrayList<>();
-        for (int i = 0; i < source.length(); i += length) {
-            int endIdx = Math.min(i + length, source.length());
-            tokens.add(source.substring(i, endIdx));
+        for (int i = 1; i <= s.length(); i++) {
+            StringBuilder sb = new StringBuilder();
+            
+            // i개 단위로 자른다.
+            String prev = s.substring(0, i);
+            int cnt = 1;
+            
+            for (int j = i; j < s.length(); j += i) {
+                int endIdx = Math.min(j + i, s.length());
+                String current = s.substring(j, endIdx);
+                
+                if (prev.equals(current)) {
+                    cnt++;
+                } else {
+                    if (cnt > 1) {
+                        sb.append(cnt);
+                    }
+                    sb.append(prev);
+                    
+                    prev = current;
+                    cnt = 1;
+                }
+            }
+            
+            if (cnt > 1) {
+                sb.append(cnt);
+            }
+            sb.append(prev);
+            
+            answer = Math.min(answer, sb.length());
         }
         
-        return tokens;
-    }
-
-    private int compress(String source, int length) {
-        StringBuilder sb = new StringBuilder();
-
-        String prev = "";
-        int cnt = 0;
-        for (String token : splitToken(source, length)) {
-            if (token.equals(prev)) {
-                cnt++;
-            } else {
-                if (cnt > 1) {
-                    sb.append(cnt);
-                }
-                sb.append(prev);
-                prev = token;
-                cnt = 1;
-            }
-        }
-
-        if (cnt > 1) {
-            sb.append(cnt);
-        }
-        sb.append(prev);
-
-        return sb.length();
-    }
-
-    public int solution(String s) {
-        int answer = 0;
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= s.length(); i++) {
-            int result = compress(s, i);
-            if (result < min) {
-                min = result;
-            }
-        }
-
-        answer = min;
         return answer;
     }
 }
