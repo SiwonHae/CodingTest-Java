cd4206a9da12a238ab4d57dd26fb7cb43361911b
Juhyuk Lim <705lo@naver.com>
2025-11-19 22:18:06 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16472 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -5,28 +5,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        Arrays.sort(arr);
+        
         int result = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                int sum = arr[i] + arr[j];
-
-                if (sum == M) {
-                    result++;
-                }
+        int start = 0;
+        int end = N - 1;
+        
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+            
+            if (sum == M) {
+                result++;
+                start++;
+                end--;
+            } else if (sum < M) {
+                start++;
+            } else {
+                end--;
             }
         }
-
+        
         bw.write(result + "");
         bw.flush();
     }
